c7c4afd7ecdfcfb6b138519dc587047d347fe0a1
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2025-03-03 09:41:46 +0100

    No pointers in signatures

--- bson/bson.ml
+++ bson/bson.ml
@@ -3,21 +3,22 @@ open Ctypes
 type t = Types_generated.t structure
 let t = Types_generated.t
 
-let ptr_t = Ctypes_static.ptr t
-
-let carray_to_string (arr : char CArray.t) : string =
-  arr |> CArray.to_list |> List.to_seq |> String.of_seq
-
 module Error = struct
-  type t = Types_generated.Error.t
+  type t = (Types_generated.Error.t, [`Struct]) structured
   let t = Types_generated.Error.t
-  let domain error =
+  let domain (error : t) =
     getf error Types_generated.Error.domain |> Unsigned.UInt32.to_int
-  let code error = getf error Types_generated.Error.code |> Unsigned.UInt32.to_int
-  let message error = getf error Types_generated.Error.message |> carray_to_string
+  let code error =
+    getf error Types_generated.Error.code |> Unsigned.UInt32.to_int
+  let message error =
+    getf error Types_generated.Error.message
+    |> CArray.to_list
+    |> List.to_seq
+    |> String.of_seq
 end
 
-let new_from_json ?len data : (t ptr, string) result =
+(* ( const uint8_t * ) *)
+let new_from_json ?len data : (t, string) result =
   let len = PosixTypes.Ssize.(Option.fold ~none:minus_one ~some:of_int len) in
   let json = Ctypes_std_views.char_ptr_of_string data in
   let json = coerce (ptr char) (ptr (const uint8_t)) json in
@@ -26,12 +27,14 @@ let new_from_json ?len data : (t ptr, string) result =
   if is_null bson then
     Result.Error ("JSON parsing Error: " ^ Error.message error)
   else
-    Result.Ok bson
+    Result.Ok !@bson
 
-let as_json ?length (bson : t ptr) =
+(*  ( const bson_t * )  *)
+let as_json ?length (bson : t) =
+  let ptr = allocate t bson in
   let length =
     let some u = u |> Unsigned.Size_t.of_int |> allocate size_t in
     Option.fold ~none:(from_voidp size_t null) ~some length in
-  let str = C.Functions.as_json bson length in
+  let str = C.Functions.as_json ptr length in
   Ctypes_std_views.string_of_char_ptr str
   (* TODO: C.Functions.free (to_voidp str) *)
--- mongoc/mongoc.ml
+++ mongoc/mongoc.ml
@@ -5,14 +5,15 @@ let cleanup = C.Functions.cleanup
 
 module Read_prefs = struct
   type t = Types_generated.Read_prefs.t structure Ctypes_static.ptr
+  let t = Types_generated.Read_prefs.t
 end
 
 module Cursor = struct
   type t = Types_generated.Cursor.t structure Ctypes_static.ptr
 
-  let next (cursor : t) : Bson.t ptr option =
-    let doc = allocate Bson.ptr_t (from_voidp Bson.const_t null) in
-    if C.Functions.Cursor.next cursor doc then Some !@doc else None
+  let next (cursor : t) : Bson.t option =
+    let doc = allocate (Ctypes_static.ptr Bson.t) (from_voidp (Ctypes_static.const Bson.t) null) in
+    if C.Functions.Cursor.next cursor doc then Some !@(!@doc) else None
 
   let error (cursor : t) =
     let error = make Bson.Error.t in
@@ -20,17 +21,18 @@ module Cursor = struct
       let message = Bson.Error.message error in
       Result.Error ("Cursor Error:" ^ message)
     else Ok ()
-  let destroy = C.Functions.Cursor.destroy
+  let destroy (cursor : t) = C.Functions.Cursor.destroy cursor
 end
 
 module Collection = struct
   type t = Types_generated.Collection.t structure Ctypes_static.ptr
 
-  let find_with_opts ?(opts : Bson.t ptr option) ?(read_prefs : Read_prefs.t option) (coll : t) (filter : Bson.t ptr) : Cursor.t =
-    let opts = Option.value opts ~default:(from_voidp Bson.t null) in
-    let read_prefs = Option.value read_prefs ~default:(from_voidp Types_generated.Read_prefs.t null) in
-    C.Functions.Collection.find_with_opts coll filter opts read_prefs
-  let destroy = C.Functions.Collection.destroy
+  let find_with_opts ?(opts : Bson.t option) ?(read_prefs : Read_prefs.t option) (coll : t) (filter : Bson.t) : Cursor.t =
+    let filter_ptr = allocate Bson.t filter in
+    let opts = Option.fold ~some:(allocate Bson.t) ~none:(from_voidp Bson.t null) opts in
+    let read_prefs = Option.value read_prefs ~default:(from_voidp Read_prefs.t null) in
+    C.Functions.Collection.find_with_opts coll filter_ptr opts read_prefs
+  let destroy (coll : t) = C.Functions.Collection.destroy coll
 end
 
 module Client = struct
@@ -47,7 +49,7 @@ module Client = struct
     let coll_name = char_ptr_of_string coll_name in
     C.Functions.Client.get_collection client db_name coll_name
 
-  let destroy = C.Functions.Client.destroy
+  let destroy (client : t) = C.Functions.Client.destroy client
 end
 
 
